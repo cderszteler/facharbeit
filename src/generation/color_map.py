9c8c603ce73fa932a78f8872b17603dff55149f0
qetzing <chrissmueller328@gmail.com>
2023-02-04 15:25:17 +0100

    Add code for image generation

---
+++ src/generation/color_map.py
@@ -0,0 +1,41 @@
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+
+
+def code_channel(channel):
+    return np.interp(x=channel,xp=[0,255],fp=[0,1])
+
+
+custom_data = {
+    'red': (
+        (0.0, code_channel(23), code_channel(23)),
+        (0.4, code_channel(62), code_channel(62)),
+        (0.6, code_channel(0), code_channel(0)),
+        (1.0, code_channel(0), code_channel(0))
+    ),
+    'green': (
+        (0.0, code_channel(36), code_channel(36)),
+        (0.4, code_channel(161), code_channel(161)),
+        (0.6, code_channel(229), code_channel(229)),
+        (1.0, code_channel(0), code_channel(0))
+    ),
+    'blue': (
+        (0.0, code_channel(38), code_channel(38)),
+        (0.4, code_channel(173), code_channel(173)),
+        (0.6, code_channel(225), code_channel(225)),
+        (1.0, code_channel(0), code_channel(0))
+    )
+}
+
+
+custom = LinearSegmentedColormap('custom', segmentdata=custom_data)
+
+# Code to debug color mapping:
+
+# from matplotlib import colors
+# import matplotlib.pyplot as plot
+# from matplotlib import mpl
+
+# figure, axis = plot.subplots()
+# figure.colorbar(mpl.ScalarMappable(norm=colors.Normalize(), cmap=custom), ax=axis)
+# figure.show()
---
+++ src/generation/generate.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plot
+import numpy
+from generation.color_map import custom
+
+
+def divergent_iterations(real, imaginary, max_iterations):
+    c = complex(real, imaginary)
+    z = 0.0j
+
+    for iteration in range(max_iterations):
+        z = z ** 2 + c
+        if abs(z) > 2:
+            return iteration
+
+    return max_iterations
+
+
+def generate_pixel_iterations_map(resolution, max_iterations):
+    xs = resolution
+    ys = resolution
+
+    pixels = numpy.zeros([xs, ys])
+
+    for x, real in enumerate(numpy.linspace(SCOPE[0], SCOPE[1], num=xs)):
+        for y, imaginary in enumerate(numpy.linspace(SCOPE[2], SCOPE[3], num=ys)):
+            pixels[x, y] = divergent_iterations(real, imaginary, max_iterations)
+    return pixels
+
+
+def draw_and_save_plot(pixels, color_map, size):
+    plot.figure(dpi=size)
+    plot.imshow(pixels.T, cmap=color_map, interpolation="bilinear", extent=SCOPE)
+    plot.xlabel("Re")
+    plot.ylabel("Im")
+    plot.savefig("../images/mandelbrotImage.png")
+
+
+# This value defines the quality/resolution of the image. For quick generations,
+# 500 is recommended. For higher quality images, use a value between
+# 2000-5000
+RESOLUTION = 500
+# This value defines the image size. It should be updated accordingly with the
+# resolution
+IMAGE_SIZE = RESOLUTION * 1.25
+# This value defines the scope/detail of the image. Default: [-2, 1, -1, 1]
+SCOPE = [-2, 1, -1, 1]
+# This value should be changed if the scope is not the default. Default: 100
+MAX_ITERATIONS = 100
+# This value changes the color mapping of the generated image. For a small
+# scope, "twilight" might be interesting. The default value uses
+# a custom generated color map
+COLOR_MAP = {
+    'red': (
+        (0.0, 0.09019607843137255, 0.09019607843137255),
+        (0.4, 0.24313725490196078, 0.24313725490196078),
+        (0.8, 0.0, 0.0),
+        (1.0, 0.0, 0.0)
+    ),
+    'green': (
+        (0.0, 0.1411764705882353, 0.1411764705882353),
+        (0.4, 0.6313725490196078, 0.6313725490196078),
+        (0.8, 0.8980392156862745, 0.8980392156862745),
+        (1.0, 0.0, 0.0)
+    ),
+    'blue': (
+        (0.0, 0.14901960784313725, 0.14901960784313725),
+        (0.4, 0.6784313725490196, 0.6784313725490196),
+        (0.8, 0.8823529411764706, 0.8823529411764706),
+        (1.0, 0.0, 0.0)
+    )
+}
+
+
+pixels = generate_pixel_iterations_map(RESOLUTION, MAX_ITERATIONS)
+draw_and_save_plot(pixels, custom, IMAGE_SIZE)
+print("Generated and saved image")
+
